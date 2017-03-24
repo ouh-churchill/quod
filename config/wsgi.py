cba344f09664020200664da43c23b04573033878
Carl Marshall <carl.marshall@nds.ox.ac.uk>
2017-03-24 10:19:55 +0000

    Base project setup ready to implement Wagtail in standard Django project template

---
+++ config/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for quodsite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import environ
+
+from django.core.wsgi import get_wsgi_application
+
+environ.Env.read_env(env_file='config/settings/.env')
+
+application = get_wsgi_application()
