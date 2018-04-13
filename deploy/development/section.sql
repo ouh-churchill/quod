c91bbb0bfb17f2c2f7a102d8b2650b49bc71b2dc
9711787 <tomas.surik@gmail.com>
2018-04-13 11:59:54 +0100

    Test Data
    Test data.

---
+++ deploy/development/section.sql
@@ -0,0 +1,4 @@
+INSERT INTO `research_variables_section`(`id`,`name`,`category_id`) VALUES (1,'Donor Basics(30 variables)',1);
+INSERT INTO `research_variables_section`(`id`,`name`,`category_id`) VALUES (2,'Trauma, indicating injuries (6 variables)',1);
+INSERT INTO `research_variables_section`(`id`,`name`,`category_id`) VALUES (3,'Patient measures (5 variables)',1);
+INSERT INTO `research_variables_section`(`id`,`name`,`category_id`) VALUES (1,'Blood group including, where known, subtypes of A (3 variables)',1);
